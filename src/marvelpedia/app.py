827b6a6a84d30cc7099c2e898f82dc1762efdecb
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2021-04-02 17:58:22 -0300

    base and comicsResource class implemented

--- src/marvelpedia/app.py
+++ src/marvelpedia/app.py
@@ -5,41 +5,34 @@ import requests
 from .key_manager import get_hash_ts
 
 
-class CharactersResource(object):
-    def on_get(self, req, resp, character_id=None):
-        if character_id is not None:
-            rf = ["id", "name", "description", "comics"]
-        else:
-            rf = ["id", "name"]
-
-        characters = self.get_characters(character_id, rf)
-        resp.body = json.dumps(characters)
-        resp.status = falcon.HTTP_200
-
-    def get_characters(self, character_id, required_fields, apply_filter=True) -> dict:
-        s = f"/{character_id}" if character_id else ""
+class BaseResource(object):
+    def get_content(self, content_id, required_fields, content_type, apply_filter=True) -> dict:
+        s = f"/{content_id}" if content_id else ""
         my_hash, ts = get_hash_ts()
+        if content_type not in ("characters", "comics"):
+            raise TypeError("content_type must be 'characters' or 'comics'")
 
-        url = "https://gateway.marvel.com/v1/public/" \
-              "characters" + s + \
+        url = "https://gateway.marvel.com/v1/public/" + \
+              content_type + s + \
               "?ts=" + str(ts) + \
               "&apikey=947107e18f1a91614288b3e65c30d5ae" \
               "&hash=" + my_hash
         r = requests.get(url)
 
         if apply_filter:
-            character_json = self.filter_required_fields(
+            content_json = self.filter_required_fields(
                 json.loads(r.content)['data']["results"],
                 required_fields
             )
         else:
-            character_json = json.loads(r.content)['data']["results"]
+            content_json = json.loads(r.content)['data']["results"]
 
-        return character_json
+        return content_json
 
-    def filter_required_fields(self, characters_list, required_fields) -> list:
+    @staticmethod
+    def filter_required_fields(comics_list, required_fields) -> list:
         filtered_list = list()
-        for character in characters_list:
+        for character in comics_list:
             filtered_character = dict()
             for field in required_fields:
                 filtered_character[field] = character[field]
@@ -47,7 +40,35 @@ class CharactersResource(object):
         return filtered_list
 
 
+class CharactersResource(BaseResource):
+    def on_get(self, req, resp, character_id=None):
+        if character_id is not None:
+            rf = ["id", "name", "description", "comics"]
+        else:
+            rf = ["id", "name"]
+
+        my_characters = self.get_content(character_id, rf, "characters")
+        resp.body = json.dumps(my_characters)
+        resp.status = falcon.HTTP_200
+
+
+class ComicsResource(BaseResource):
+    def on_get(self, req, resp, comic_id=None):
+        if comic_id is not None:
+            rf = ["id", "title", "variantDescription", "characters"]
+        else:
+            rf = ["id", "title"]
+
+        comics = self.get_content(comic_id, rf, "comics")
+        resp.body = json.dumps(comics)
+        resp.status = falcon.HTTP_200
+
+
 app = falcon.API()
 characters = CharactersResource()
+comics = ComicsResource()
+
 app.add_route('/characters/{character_id}', characters)
 app.add_route('/characters/', characters)
+app.add_route('/comics/{comic_id}', comics)
+app.add_route('/comics/', comics)
