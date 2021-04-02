5aed2e481353f16ef13f7d2646c6d60f000fd336
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2021-04-01 23:56:42 -0300

    CharactersResource implemented

---
+++ src/marvelpedia/app.py
@@ -0,0 +1,48 @@
+import falcon
+import json
+import requests
+
+
+class CharactersResource(object):
+    def on_get(self, req, resp, character_id=None):
+        if character_id is not None:
+            rf = ["id", "name", "description", "comics"]
+        else:
+            rf = ["id", "name"]
+        characters = self.get_characters(character_id, rf)
+
+        resp.body = json.dumps(characters)
+        resp.status = falcon.HTTP_200
+
+    def get_characters(self, character_id, required_fields, apply_filter=True) -> dict:
+        s = f"/{character_id}" if character_id else ""
+        url = "https://gateway.marvel.com/v1/public/" \
+              "characters" + s + \
+              "?ts=1&apikey=947107e18f1a91614288b3e65c30d5ae" \
+              "&hash=edbb5c9488c3367843bd32c150dcd52f"
+        r = requests.get(url)
+
+        if apply_filter:
+            character_json = self.filter_required_fields(
+                json.loads(r.content)['data']["results"],
+                required_fields
+            )
+        else:
+            character_json = json.loads(r.content)['data']["results"]
+
+        return character_json
+
+    def filter_required_fields(self, characters_list, required_fields) -> list:
+        filtered_list = list()
+        for character in characters_list:
+            filtered_character = dict()
+            for field in required_fields:
+                filtered_character[field] = character[field]
+            filtered_list.append(filtered_character)
+        return filtered_list
+
+
+app = falcon.API()
+characters = CharactersResource()
+app.add_route('/characters/{character_id}', characters)
+app.add_route('/characters/', characters)
